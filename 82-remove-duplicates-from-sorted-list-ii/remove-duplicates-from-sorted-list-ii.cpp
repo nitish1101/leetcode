d0751bc6c9ede0c3650ed57e5909450bfff273cb
Nitish Gupta <nitishgupta1101@gmail.com>
2024-04-24 02:23:24 +0530

    Time: 5 ms (63.31%) | Memory: 14 MB (93.28%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -1,26 +1,3 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
- /***
-
-0  1 2 3 3 3 4 4 5
-     *     #  
-   1 2------>4 4 5 
-     *         #
-   1 2---------->5->N
-     *(*)           #
-                 *  #(#->NXT && #)
-                 *=*->NXT 
-
-   1 2 5
- */
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
