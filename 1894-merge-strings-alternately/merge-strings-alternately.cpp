266022681cf7f9b2873d2091cd3e18e17e8d601f
Nitish Gupta <nitishgupta1101@gmail.com>
2025-01-03 19:25:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (54.84%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -5,18 +5,15 @@ public:
     string mergeAlternately(string word1, string word2) {
         int l1=word1.length();
         int l2=word2.length();
-        int i=0,j=0;
+        
         string ans="";
-        while(i<l1 && j<l2)
+        for(int i=0;i<max(l1,l2);i++)
         {
-            ans+=word1[i];
-            ans+=word2[j];
-            i++;j++;
+            if(i<l1)
+                ans+=word1[i];
+            if(i<l2)
+                ans+=word2[i];
         }
-        if(i<l1)
-            ans+=word1.substr(i,l1);
-        if(j<l2)
-            ans+=word2.substr(j,l2);
        return ans;
     }
 };
