4da331960b3693c1c05259f96696004b7e930ff3
Nitish Gupta <nitishgupta1101@gmail.com>
2026-06-24 23:01:53 +0530

    Time: 3 ms (40.61%) | Memory: 8.5 MB (31.81%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,19 +1,19 @@
-#include<bits/stdc++.h>
-using namespace std;
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int l1=word1.length();
-        int l2=word2.length();
-        
-        string ans="";
-        for(int i=0;i<max(l1,l2);i++)
-        {
-            if(i<l1)
-                ans+=word1[i];
-            if(i<l2)
-                ans+=word2[i];
+        int m = word1.size();
+        int n = word2.size();
+        string result = "";
+
+        for (int i = 0; i < max(m, n); i++) {
+            if (i < m) {
+                result.push_back(word1[i]);
+            }
+            if (i < n) {
+                result.push_back(word2[i]);
+            }
         }
-       return ans;
+
+        return result;
     }
 };
