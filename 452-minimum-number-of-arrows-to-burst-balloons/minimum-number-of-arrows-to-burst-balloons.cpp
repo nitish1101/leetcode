8f15a50be310df8a6292284c005c2fe1a35d96e8
Nitish Gupta <nitishgupta1101@gmail.com>
2026-08-14 17:41:41 +0530

    Time: 56 ms (24.75%) | Memory: 96.7 MB (12.94%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,22 +1,29 @@
 class Solution {
+
+        static bool cmp(vector<int>& a, vector<int>& b) {
+            return a[0] < b[0];
+        }
+
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
-        sort(points.begin(),points.end());
-        int n=points.size();
-        int c=0;
-        for(int i=0;i<n;)
-        {
-            int os=points[i][0];
-            int oe=points[i][1];
-            int j=i+1;c++;
-            while(j<n && points[j][0] <= oe){
-                    os=points[j][0];
-                if(points[j][1] < oe)
-                    oe=points[j][1];
-                j++;
+        int n = points.size();
+        sort(points.begin(), points.end(), cmp);
+
+        stack<pair<int, int>> s;
+        s.push({points[0][0], points[0][1]});
+
+        int j = 1;
+        while (!s.empty() && j < n) {
+            pair<int, int> p = s.top();
+            if (points[j][0] <= p.second) {
+                s.pop();
+                s.push({max(p.first, points[j][0]), min(p.second, points[j][1])});
             }
-            i=j;
+            else
+                s.push({points[j][0], points[j][1]});
+            j++;
         }
-        return c;
+        return s.size();
     }
 };
+
