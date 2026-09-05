06a84c50c015a5088a7c717c93c98ee454406352
Nitish Gupta <nitishgupta1101@gmail.com>
2026-09-05 18:00:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (40.23%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,26 +1,69 @@
 class Solution {
-    vector<vector<int>> ans;
 public:
-    void f(int i, int target, int k, vector<int>& tmp)
-    {
-        if(target < 0)
+    void rec(int i, int k, int t, vector<int>& ds, vector <vector<int>> &ans) {
+        if (t==0 && k==0) {
+            ans.push_back(ds);
             return;
-        if(k==0)
-        {
-            if(target==0)
-                ans.push_back(tmp);
-            return;
-        }
-        for(int x=i;x<=9;x++){
-            tmp.push_back(x);
-            f(x+1,target-x,k-1,tmp);
-            tmp.pop_back();
         }
 
+        if (t < 0 || k==0 || i>9)
+            return;
+        
+        //pick
+        ds.push_back(i);
+        rec(i+1,k-1,t-i,ds,ans);
+        ds.pop_back();
+
+        //np
+        rec(i+1,k,t,ds,ans);
     }
-    vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> tmp;  
-        f(1,n,k,tmp);
+
+    vector<vector<int>> combinationSum3(int k, int target) {
+        vector<int> ds;
+        vector <vector<int>> ans;
+        rec(1,k,target,ds,ans);
         return ans;
     }
 };
+
+/**
+
+
+[1,2,...9]
+atmost once
+k=3
+t=7
+
+1,2,4 , k=3
+
+1,6
+
+2,5
+
+3,4
+
+
+
+f(i,k,t,ds,ans)
+
+
+if(!t && !k))
+    ans.add(ds);
+    return;
+if(t<0)
+    return;
+if(!k)
+    return;
+if(i>9)
+    return;
+
+//pick
+ds.add(i);
+rec(i+1,k-1,t-i,ds,ans);
+ds.remove(i);
+
+//not pick
+rec(i+1,k,t,ds,ans);
+
+
+*/
