b253fee845b57a159b471c3661d06efb6ef91d95
Nitish Gupta <nitishgupta1101@gmail.com>
2026-09-05 17:32:54 +0530

    Time: 2 ms (57.53%) | Memory: 14 MB (93.43%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,35 +1,44 @@
 class Solution {
 public:
-     set<vector<int>> ans;
-     vector<vector<int>> res;
-     void f(vector<int>& nums,int target, vector<int>& tmp)
+    void rec(int i, int t, vector<int>& cd, vector<int>& comb, vector<vector<int>>& ans)
     {
-        if(target<0)
-            return;
-        if(target==0)
+        if(t==0)
         {
-            vector<int> v(tmp.begin(),tmp.end());
-            sort(v.begin(),v.end());
-            if(ans.find(v)==ans.end()){
-                ans.insert(v);
-                res.push_back(v);
-            }
+            ans.push_back(comb);
             return;
+        }
+        if(t<0)
+            return;
+
+        for(int x=i;x<cd.size();x++)
+        {
+            comb.push_back(cd[x]);
+            rec(x,t-cd[x],cd,comb,ans);
+            comb.pop_back();
         }
 
-        for(int i=0;i<nums.size();i++)
-        {
-                tmp.push_back(nums[i]);
-               
-                f(nums,target-nums[i],tmp);
-                
-                tmp.pop_back();
-        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> comb;
+        vector<vector<int>> ans;
+        rec(0,target,candidates, comb,ans);
+        return ans;
         
     }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> tmp;
-        f(candidates,target,tmp);
-        return res;
-    }
 };
+
+
+
+/**
+2,3,6,7,    7
+
+[2.],5,     [3,],4.     [6],
+    [2.2],3 -> [2.2.2],1 -> xx
+    [2.3],2 -> [2.3.2],0 -> return [2.3.2] xx  
+    xx
+
+
+
+
+*/
